96c0ee007c4ed6d509a6228bb0484820e97d9c83
Heritiana Rivoarison <heritiana.rivoarison@sncf.fr>
2024-12-06 18:16:43 +0100

    first solution

---
+++ 1/1.py
@@ -0,0 +1,33 @@
+
+def readFile(path):
+    col1 = []
+    col2 = []
+
+    with open(path, 'r') as file:
+        for line in file:
+            # Split the line into two parts
+            parts = line.strip().split()
+            if len(parts) == 2:  # Ensure the line has two columns
+                col1.append(parts[0])
+                col2.append(parts[1])
+    return col1, col2
+
+def getMinIndex(arr):
+    minim = min(arr)
+    index = arr.index(minim)
+    return int(minim), index
+
+
+file_path = "/Users/FHRN01241/Documents/advent-code-2024/1/input 1.txt"
+arr1, arr2 = readFile(file_path)
+
+sum = 0
+while(len(arr1) > 0):
+    min1, index1 = getMinIndex(arr1)
+    min2, index2 = getMinIndex(arr2)
+
+    sum += abs(min1 - min2)
+    arr1.pop(index1)
+    arr2.pop(index2)
+
+print(sum)
